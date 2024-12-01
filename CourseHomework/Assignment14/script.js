9525b1ab2bf906957d7a7b434e34b07f6bf64079
Michael-Finley <mf199706@umconnect.umt.edu>
2024-12-01 13:20:27 -0700

    Create script.js

---
+++ CourseHomework/Assignment14/script.js
@@ -0,0 +1,20 @@
+// Simple animation for the canvas
+const canvas = document.getElementById('animationCanvas');
+const ctx = canvas.getContext('2d');
+
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+const ballRadius = 10;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#FF0000";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 
